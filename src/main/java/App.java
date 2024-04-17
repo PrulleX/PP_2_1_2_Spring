6c22c3fab29858a8f19debed04f9a27bcd925e97
PrulleX <safronovvanya2020@gmail.com>
2024-04-17 15:51:34 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,30 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
